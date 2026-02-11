f7bbfd281861d43995a3da405f51c4f380e552c2
William Torman <willtorman@gmail.com>
2026-02-10 22:00:59 -0700

    U1 routing done

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
+import { fileURLToPath } from "url";
+import path from "path";
 // Import express using ESM syntax
 import express from "express";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Create an instance of an Express application
 const app = express();
 
-const name = process.env.NAME;
+/**
+ * Configure Express middleware
+ */
 
-// Define a route handler for the root URL ('/')
-app.get("/", (req, res) => {
-  res.send(`Hello, ${name}!`);
-});
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+const name = process.env.NAME;
 
 // Define the port number the server will listen on
 const PORT = 3000;
@@ -17,4 +24,19 @@ const PORT = 3000;
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
+});
+
+/**
+ * Routes
+ */
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
 });
